33a584d16ec961e9b85a102c33711499966391e4
Narayan-Sambare <sambarenarayan@gmail.com>
2025-03-29 23:10:25 +0530

    update date

---
+++ gemini.py
@@ -0,0 +1,52 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyDUZ5L1KMv6xMmUvQ3-ffr63xVCeNAHrPw")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 256,
+  
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro-001",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[])
+
+while True:
+    user_input = input("Enter you query: ")
+    print(f"You: {user_input}")
+    if user_input.lower() == "exit":
+      print("Goodbye!")
+      break
+    convo.send_message(user_input)
+    print(convo.last.text)
